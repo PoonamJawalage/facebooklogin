6aebd9b630d5746683fe303bccef3e66f078235e
Pipl-10 <pipl-10@Pipl-10s-iMac.local>
2017-07-31 12:33:47 +0530

    Initial Commit

---
+++ fb_login/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fb_login
+//
+//  Created by Pipl-10 on 31/07/17.
+//  Copyright © 2017 Pipl-10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fb_login/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fb_login
+//
+//  Created by Pipl-10 on 31/07/17.
+//  Copyright © 2017 Pipl-10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
